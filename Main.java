6bfdb522719f1d77fa84e4882048b165cd48d076
DennisAlwyn <104767447+DennisAlwyn@users.noreply.github.com>
2024-02-10 19:34:02 +0800

    Create Main class and load empty window

---
+++ Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main{
+    public static void main(String[] args){
+        
+        //Create Window with JFrame
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Group 5 Particle Simulator");
+        frame.setSize(1280, 720);
+        frame.setVisible(true);
+    }
+}
