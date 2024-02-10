c1358da0c700125a96ccee3dce953303c32df0bc
DennisAlwyn <104767447+DennisAlwyn@users.noreply.github.com>
2024-02-10 20:40:03 +0800

    Created a class that represents a particle
    Note: May not work later. Hopefully it does

---
+++ Particle.java
@@ -0,0 +1,52 @@
+import java.math.*;
+
+public class Particle {
+    public double x, y; //Coordinates of particle
+    private double xRef, yRef; //Coordinates of reflection
+    private int waitTime; //How long should this particle wait before updating in nanoseconds
+    private int lastUpdate; //System.nanotime of last time the particle was updated
+    private int angle; //Angle that the particle is moving
+    private boolean hasReflected; //Checks if particle has already reflected to prevent multiple relfections
+
+    //Constructor
+    public Particle(int xpos, int ypos, int speed, int theta){
+        x = xpos;
+        y = ypos;
+        angle = theta;
+        lastUpdate = 0;
+        waitTime = 1000000000 / speed; //Divides 1 second in ns by speed to get movement speed
+        hasReflected = false;
+
+        //Set these to impossible coords so that it doesn't cause errors maybe...
+        xRef = -420;
+        yRef = -420;
+    }
+
+    private void move(){ //Method for moving the particle
+        if((System.nanoTime() - lastUpdate) >= waitTime){ //If it is time for the particle to move
+            x += Math.cos(angle); //Add cosine of angle to get x movement
+            y += Math.sin(angle)*-1; //Add sine of angle to get y movement. Multiple by -1 since x axis is flipped
+
+            if(Math.abs(xRef-x) >= 1 && Math.abs(yRef-y) >= 1){ //Check if the particle has moved away from the wall
+                hasReflected = false; //Particle has moved sufficiently, no longer reflecting
+                //Reset values
+                xRef = -420;
+                yRef = -420;
+            }
+        }
+    }
+
+    private void reflect(){ //Determines new angle on reflection
+        if(!hasReflected){ //Particle has not yet reflected
+            hasReflected = true; //Particle is now reflecting
+            
+            //Record reflection coordinates
+            xRef = x;
+            yRef = y;
+
+            angle += 90;
+            if(angle >= 360) //Reduce by 1 full circle if angle >= 360
+                angle -= 360;
+        }
+    }
+}
