9052d4025673c6d84f3f65e373ef55ea2509f2b8
DennisAlwyn <104767447+DennisAlwyn@users.noreply.github.com>
2024-02-10 22:31:05 +0800

    Fix to match specs

--- Main.java
+++ Main.java
@@ -4,15 +4,24 @@ import java.util.ArrayList;
 
 public class Main{
     //Constants for sizes
-    public static final int FRAME_WIDTH = 1280;
-    public static final int FRAME_HEIGHT = 720;
-    public static final int SIZE = 650;
+    public static final int FRAME_WIDTH = 1600;
+    public static final int FRAME_HEIGHT = 900;
+    public static final int SIM_WIDTH = 1280;
+    public static final int SIM_HEIGHT = 720;
 
     //JFrame
     public static JFrame frame;
 
     //JPanels
     public static SimulatorPanel simulator;
+
+    //Threads
+    public static Thread rendererThread;
+
+    //FPS
+    public static long lastFPSCheck = 0;
+    public static int currentFPS = 0;
+    public static int totalFrames = 0;
 
     //ArrayLists of walls and particles
     public static ArrayList<Particle> particles = new ArrayList<>();
@@ -28,7 +37,7 @@ public class Main{
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setVisible(true);
 
-        //ADD PANELS
+        //Add Simulator Panel
         simulator = new SimulatorPanel();
         frame.add(simulator);
     }
@@ -37,7 +46,7 @@ public class Main{
 
         public SimulatorPanel(){
             this.setBackground(Color.BLACK);
-            this.setBounds(50, FRAME_HEIGHT/2-SIZE/2-25, SIZE, SIZE);
+            this.setBounds(10, FRAME_HEIGHT/2-SIM_HEIGHT/2-50, SIM_WIDTH, SIM_HEIGHT);
         }
 
         public void paintComponent(Graphics g){
@@ -47,12 +56,25 @@ public class Main{
             //Cast grahics to g2D
             Graphics2D g2D = (Graphics2D) g;
 
+            //Clear screen
+            g2D.setColor(Color.BLACK);
+            g2D.fillRect(0, 0, SIM_WIDTH, SIM_HEIGHT);
+
             //Set Draw Color to Green
             g2D.setColor(Color.GREEN);
 
             //Draw stuff. Remove Later
             g2D.drawLine(0, 0, 200, 200); //Wall Example
             g2D.drawOval(100, 10, 3, 3); //Particle Example
+        }
+    }
+
+    public static class rendererObject extends Thread{
+
+        public void run(){
+            while(true){
+
+            }
         }
     }
 }
